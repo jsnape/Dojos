b6186dac58a14c8ef00eea08ef39a06d134dd0ba
James Snape <james.snape@avanade.com>
2024-11-28 16:53:57 +0000

    Initial commit

---
+++ WordSearch/Grid.cs
@@ -0,0 +1,17 @@
+﻿namespace WordSearch;
+
+internal class Grid(int Width, int Height)
+{
+    private readonly char[,] grid = new char[Width, Height];
+
+    public char this[int x, int y]
+    {
+        get => grid[x, y];
+        set => grid[x, y] = value;
+    }
+
+    public int Width => grid.GetLength(0);
+
+    public int Height => grid.GetLength(1);
+}
+
---
+++ WordSearch/GridFacts.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace WordSearch;
+
+public class GridFacts
+{
+    private readonly Grid target = new(2, 3);
+
+    [Fact]
+    public void CanCreateGrid()
+    {
+        Assert.NotNull(target);
+    }
+
+    [Fact]
+    public void DimensionsAreCorrect()
+    {
+        Assert.Equal(2, target.Width);
+        Assert.Equal(3, target.Height);
+    }
+}
